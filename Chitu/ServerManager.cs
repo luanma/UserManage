cab5d86f95c1e5332079568df3de1a70688eb2d0
lee.zhang <lee.zhang@thomsonreuters.com>
2018-01-17 13:28:54 +0800

    Add client

---
+++ Chitu/ServerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UserManage.Model;
+
+namespace Chitu
+{
+    class ServerManager
+    {
+        public List<ServerInfo> GetServer()
+        {
+            //List<ServerInfo> servers = new List<ServerInfo>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:61940/api/server/getmyservers");
+            client.DefaultRequestHeaders.Accept.Add(
+           new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<DataObject>>().Result;
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            return null;
+        }
+
+        
+    }
+}
