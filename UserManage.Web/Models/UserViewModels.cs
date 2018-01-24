de1cc7401fa0f153d8c191bd7ce21fa2effca6a2
lee.zhang <lee.zhang@thomsonreuters.com>
2018-01-24 13:51:44 +0800

    Update create user.

--- UserManage.Web/Models/UserViewModels.cs
+++ UserManage.Web/Models/UserViewModels.cs
@@ -9,18 +9,15 @@ namespace UserManage.Web.Models
 {
     public class UserViewModels
     {
-        [Required]
+
         [Display(Name="手机号")]
         [DataType(DataType.PhoneNumber)]
-        [MaxLength(11,ErrorMessage ="最长11位")]
-        
         public string UserName { get; set; }
 
-        [Required]
-        [Display(Name ="密码")]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
-        [Required]
+        
         [Display(Name = "验证码")]
         public string SMSStr { get; set; }
     }
