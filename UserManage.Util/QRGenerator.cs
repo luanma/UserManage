a9eac06ded047eb05d284608ad48ca27a3d8f173
lee.zhang <lee.zhang@thomsonreuters.com>
2018-02-05 13:37:50 +0800

    Add QRcode support

---
+++ UserManage.Util/QRGenerator.cs
@@ -0,0 +1,23 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManage.Util
+{
+    public static class QRGenerator
+    {
+       static  QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        public static Bitmap GenerateQRCode(string source)
+        {
+            
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(source, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+            return qrCodeImage;
+        }
+    }
+}
---
+++ UserManage.UtilTests/QRGeneratorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserManage.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace UserManage.Util.Tests
+{
+    [TestClass()]
+    public class QRGeneratorTests
+    {
+        [TestMethod()]
+        public void GenerateQRCodeTest()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for(int i=0;i<100;i++)
+            {
+                string s = "weixin：//wxpay/s/" + System.Guid.NewGuid().ToString().Substring(4);
+                Bitmap b = QRGenerator.GenerateQRCode(s);
+            }
+            sw.Stop();
+            System.Diagnostics.Debug.Print(sw.ElapsedMilliseconds.ToString());
+            Assert.IsTrue(sw.ElapsedMilliseconds < 3000);
+        }
+    }
+}
